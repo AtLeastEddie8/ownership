c362c358eaa085083638b14d3d2de073c5f09b6e
AtLeastEddie8 <ee3de33@icloud.com>
2023-11-14 17:20:54 -0500

    added the string type

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,17 @@ fn main() {
     }
     //out of scope
     //println!("{r}");
+    
+    //The String Type 
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
 
+    // println!("{s}"); //same as above but no grapple
+
+    //this can be used to reassign the value of a string once it is mutable 
+    s = String::from("yo");
+    println!("{s}");
+    //Variables and Data Interacting with Move
+    
 }
