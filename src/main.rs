e735da213df77f9b5c2184d7c59a7f4b3ab7149e
AtLeastEddie8 <ee3de33@icloud.com>
2023-11-14 18:45:38 -0500

    added moved and clone examples

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,35 @@ fn main() {
     println!("{s}");
     //Variables and Data Interacting with Move
     
+    let x = 5;
+    let _y = x;
+
+    let s1 = String::from("Elephant");
+    let s2 = s1;
+    //this wont work bc s1 was moved to s2, therefore s1 no longer exists
+    //println!("s1 = {}, s2 = {}", s1, s2);
+
+    //this works bc tha value is cloned (copied) instead of being moved
+    let s3 = s2.clone();
+    println!("s2 = {}, s3 = {}", s2, s3);
+
+    let f = String::from("connection");
+    moved_owner(f);
+    //this wont work bc the ownership was moved to the function call
+    //and once the fcuntion went out of scope the value no longer existed
+    //println!("{} is the moved string no longer existant", f);
+
+    let number = 34;
+    making_copy(number);
+    println!("{} is the original number still existant", number)
+    
+    
+}
+
+fn moved_owner(a_string: String){
+    println!("{} was the string moved here, will go out of scope and original non-existant", a_string);
+}
+
+fn making_copy(num: i32){
+    println!("{} is the number copied, it will exist after still", num);
 }
