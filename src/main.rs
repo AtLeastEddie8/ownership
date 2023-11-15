aa65cab1a812eb9fae2edf0311435945a4cae9d6
AtLeastEddie8 <ee3de33@icloud.com>
2023-11-14 19:27:08 -0500

    added example of borrow w/o transferring ownership

--- src/main.rs
+++ src/main.rs
@@ -42,15 +42,63 @@ fn main() {
 
     let number = 34;
     making_copy(number);
-    println!("{} is the original number still existant", number)
+    println!("{} is the original number still existant", number);
     
+    {
+        let mut f2 = String::from("secured");
+        f2.push_str(" for now");
+        println!("The value of f2 prior to move is '{}'", f2);
+        //moved so f2 not valid anymore
+        let f3 = f2;
+        //f2 made valid again by making a new string, f3 now invalid
+        let f2 = f3;
+        //f2 made invalid again bc of fxn call
+        println!("f2 is once again '{}'", f2);
+        let f5 = moved_owner_and_returned(f2);
+        println!("f5 is '{}'", f5);
+
+        let f4 = String::from("closing");
+        //f2 made valid once again from f4, f4 now invalid
+        let f2 = String::from(moved_owner_and_returned(f4));
+        println!("The value of f2 is now '{}'", f2);
+        //will get error bc f4 was moved and its being borrowed
+        //println!("The value of f4 is now '{}'", f4);
+    }
+    //example where instead of move the fxn borrows and the string is not invalid 
+    {
+        //ping-ponging ownership f2->f3->f2
+        let f2 = String::from("Vampire");
+        println!("f2 originally '{}'", f2);
+        let f3 = f2;
+        let f2 = f3;
+        println!("f2 is still '{}'", f2);
+        //new mutable string that will change value and then NOT pass ownership
+        let mut f4 = f2;
+        println!("f4 before change is '{}'", f4);
+        f4 = String::from("Wolf");
+        //wont work bc of prev command
+        //println!("f2 is '{}', ", f2);
+        let f2 = moved_owner_and_returned_borrow(&f4);
+        println!("f2 revived again as '{}' and f4 is '{}' bc of mut", f2, f4);
+    }
+
     
 }
 
 fn moved_owner(a_string: String){
-    println!("{} was the string moved here, will go out of scope and original non-existant", a_string);
+    println!("'{}' was the string moved here, will go out of scope and original non-existant", a_string);
 }
 
 fn making_copy(num: i32){
-    println!("{} is the number copied, it will exist after still", num);
+    println!("'{}' is the number copied, it will exist after still", num);
+}
+
+fn moved_owner_and_returned(a_string: String)->String{
+    println!("In this function the ownership of '{}' returns bc of 'return'", a_string);
+    return a_string;
+}
+
+fn moved_owner_and_returned_borrow(a_string: &String)->&String{
+    println!("In this function the ownership of '{}' was borrowed", a_string);
+    return a_string;
 }
